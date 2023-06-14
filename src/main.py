9dbaa8d36fff9e798490822986c535beb12148f3
Ciprian <botnari_ciprian@mail.ru>
2023-06-14 09:15:13 +0300

    Command line argument

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+import sys
 from lexer import Lexer
 from parser import Parser
 from fractal import Fractal
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("[USAGE]: py main.py file_name.txt")
+        sys.exit(1)
+    
+    filename = sys.argv[1]
     fractal = Fractal()
 
-    with open('source.txt', 'r') as file:
+    with open(filename, 'r') as file:
         text = file.read()
 
     lexer = Lexer(text)
